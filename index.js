228cf7cb8a24177d66b80329970fa61acb2206b4
Kaitokien <luutrungkien107@gmail.com>
2024-09-07 21:43:31 +0700

    Đổi trạng thái công việc

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const database = require('./config/database');
+const bodyParser = require('body-parser');
 require('dotenv').config();
 
 // Routes Version 1
 const routesVer1 = require('./api/v1/routes/index_routes')
+
 database.connect();
 
 const app = express();
 const port = process.env.PORT;
+
+app.use(bodyParser.json());
 
 routesVer1(app);
 
