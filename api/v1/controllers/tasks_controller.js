81f2535128f10b886409d07330c4c0d52985ff6c
Kaitokien <luutrungkien107@gmail.com>
2024-09-07 20:40:47 +0700

    Phân trang

--- api/v1/controllers/tasks_controller.js
+++ api/v1/controllers/tasks_controller.js
@@ -1,4 +1,5 @@
 const Task = require('../models/tasks_model');
+const paginationHelper = require('../../../helpers/pagination');
 
 // [GET] /api/v1/tasks
 module.exports.index = async (req, res)  => {
@@ -10,7 +11,31 @@ module.exports.index = async (req, res)  => {
     find.status = req.query.status
   }
   // Het bo loc
-  const tasks = await Task.find(find);
+
+  // Pagination
+  const countTasks = await Task.countDocuments(find);
+  let objectPagination = paginationHelper(
+    {
+      currentPage: 1,
+      limitItems: 2
+    },
+    req.query,
+    countTasks
+  );
+
+  // End Pagination
+
+  // Sort
+  const sort = {};
+  if(req.query.sortKey && req.query.sortValue) {
+    sort[req.query.sortKey] = req.query.sortValue;
+  }
+// End sort
+
+  const tasks = await Task.find(find)
+  .sort(sort)
+  .limit(objectPagination.limitItems)
+  .skip(objectPagination.skip);;
   res.json(tasks);
 }
 
